89c4988f4cbefd72a124c3901e249051b1986282
Judit Acs <judit@sch.bme.hu>
2017-07-11 12:32:54 +0900

    add column selection support

--- scripts/segmentation_to_standoff.py
+++ scripts/segmentation_to_standoff.py
@@ -14,37 +14,46 @@ def parse_args():
     p = ArgumentParser()
     p.add_argument('--tagging-type', choices=['BE', 'BEMS'], default='BE',
                    help="Use 2 or 4 tag tagset.")
+    p.add_argument('--columns', type=str, default="-1",
+                   help="Column to convert to BE tagging."
+                   "More columns may be specified separated by comma."
+                   "By default only the last column is converted")
     return p.parse_args()
 
 
-def tag_stream(instream, outstream, tagging_type):
+def tag_stream(instream, outstream, tagging_type, columns):
+    columns = set(int(c) for c in columns.split(','))
     for line in instream:
         try:
             line = line.decode('utf8')
         except AttributeError:
             pass
         fd = line.rstrip('\n').split('\t')
-        segmented = fd[-1]
-        segments = []
-        for segment in segmented.split(' '):
-            if tagging_type == 'BE':
-                segments.append('B{}'.format('E' * (len(segment)-1)))
-            elif tagging_type == 'BEMS':
-                if len(segment) == 1:
-                    segments.append('S')
-                else:
-                    segments.append('B{}E'.format('M' * (len(segment)-2)))
+        output_line = []
+        for i, field in enumerate(fd):
+            if i+1 in columns or (-1 in columns and i == len(fd) - 1):
+                segments = []
+                for segment in field.split(' '):
+                    if tagging_type == 'BE':
+                        segments.append('B{}'.format('E' * (len(segment)-1)))
+                    elif tagging_type == 'BEMS':
+                        if len(segment) == 1:
+                            segments.append('S')
+                        else:
+                            segments.append('B{}E'.format('M' * (len(segment)-2)))
+                output_line.append(''.join(segments))
+            else:
+                output_line.append(field)
         try:
-            outstream.write(u'{}\t{}\n'.format('\t'.join(fd[:-1]),
-                                               ''.join(segments)).encode('utf8'))
+            outstream.write(u'{}\n'.format(
+                '\t'.join(output_line)).encode('utf8'))
         except TypeError:
-            outstream.write('{}\t{}\n'.format('\t'.join(fd[:-1]),
-                                              ''.join(segments)))
+            outstream.write(u'{}\n'.format('\t'.join(output_line)))
 
 
 def main():
     args = parse_args()
-    tag_stream(stdin, stdout, tagging_type=args.tagging_type)
+    tag_stream(stdin, stdout, tagging_type=args.tagging_type, columns=args.columns)
 
 if __name__ == '__main__':
     main()
