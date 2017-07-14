0aa7eb5961c6e921c36dc6d389b200ebb8333e22
Judit Acs <judit@sch.bme.hu>
2017-07-14 02:48:04 +0200

    move hyperparam ranges to separate file. This enables runtime change

---
+++ morph_seg/sequence_tagger/hyperparam_ranges.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 Judit Acs <judit@sch.bme.hu>
+#
+# Distributed under terms of the MIT license.
+
+ranges = {
+    'embedding_size': [10, 20, 30],
+    'cell_size': [32, 64, 128, 256, 512],
+    'cell_type': ['LSTM', 'GRU'],
+    'patience': [1, 2, 5],
+}
