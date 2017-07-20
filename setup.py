bd8266a5a347e953316b4c54b3bc712c93ddb9aa
Lucy Park <me@lucypark.kr>
2017-07-20 23:21:22 +0900

    Enable semver
    -  http://semver.org/

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
     name='morph_seg',
-    version='0.1',
+    version='0.1.0',
     description="Morphological segmentation experiments",
     author='Judit Acs',
     author_email='judit@sch.bme.hu',
