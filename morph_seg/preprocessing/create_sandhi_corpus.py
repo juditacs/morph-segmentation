5250d8ca70c3237de6cc2c51633fcceabc15ac3c
Judit Acs <judit@sch.bme.hu>
2017-07-25 10:01:30 +0200

    collect INS corpus

--- morph_seg/preprocessing/create_sandhi_corpus.py
+++ morph_seg/preprocessing/create_sandhi_corpus.py
@@ -10,6 +10,7 @@ from argparse import ArgumentParser
 import json
 import gzip
 from collections import defaultdict
+from sys import stderr
 
 
 def parse_args():
@@ -59,10 +60,11 @@ predicates = {
 }
 
 
-def collect_corpus(input_files):
+def collect_corpus_stats(input_files):
     corp = defaultdict(set)
     all_words = set()
-    for infile in input_files:
+    for i, infile in enumerate(input_files):
+        stderr.write('{}/{} {}\n'.format(i+1, len(input_files), infile))
         with gzip.open(infile, 'rt') as f:
             for line in f:
                 try:
@@ -76,12 +78,33 @@ def collect_corpus(input_files):
     return corp, all_words
 
 
+def collect_corpus(input_files, keep_word):
+    corp = set()
+    for i, infile in enumerate(input_files):
+        stderr.write('{}/{} {}\n'.format(i+1, len(input_files), infile))
+        with gzip.open(infile, 'rt') as f:
+            for line in f:
+                try:
+                    token = Token.from_line(line)
+                except ValueError:
+                    continue
+                if keep_word(token):
+                    corp.add(token)
+    return corp
+
+
+def instrumental(word):
+    return word.analysis == '[/N][Ins]' and \
+            len(word.word) != len(word.lemma)
+
 def main():
     args = parse_args()
-    corp, all_words = collect_corpus(args.input_file)
-    for k, v in corp.items():
-        print(k, len(v), float(len(v)) / len(all_words))
-    print(len(all_words))
+    corp = collect_corpus(args.input_file, instrumental)
+    for word in corp:
+        print("{}\t{}".format(word.lemma, word.word))
+    #for k, v in corp.items():
+        #print(k, len(v), float(len(v)) / len(all_words))
+    #print(len(all_words))
 
     #for token in corp['lemma_change'] - corp['low_vowel_lengthening'] - corp['instrumental']:
         #print(token)
